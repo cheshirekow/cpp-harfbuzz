a108ff8505b151977aaf99b6132c1f0fc5f7700f
Josh Bialkowski <jbialk@mit.edu>
2012-08-02 13:15:21 -0400

    Added license

--- include/cppharfbuzz/Blob.h
+++ include/cppharfbuzz/Blob.h
@@ -1,4 +1,22 @@
 /*
+ *  Copyright (C) 2012 Josh Bialkowski (jbialk@mit.edu)
+ *
+ *  This file is part of cppharfbuzz.
+ *
+ *  cppharfbuzz is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  cppharfbuzz is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Fontconfigmm.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
  *  \file   Blob.h
  *
  *  \date   Jul 19, 2012
--- include/cppharfbuzz/common.h
+++ include/cppharfbuzz/common.h
@@ -1,12 +1,30 @@
 /*
+ *  Copyright (C) 2012 Josh Bialkowski (jbialk@mit.edu)
+ *
+ *  This file is part of cppharfbuzz.
+ *
+ *  cppharfbuzz is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  cppharfbuzz is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Fontconfigmm.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
  * common.h
  *
  *  Created on: Jul 18, 2012
  *      Author: josh
  */
 
-#ifndef HARFBUZZMM_COMMON_H_
-#define HARFBUZZMM_COMMON_H_
+#ifndef CPPHARFBUZZ_COMMON_H_
+#define CPPHARFBUZZ_COMMON_H_
 
 #if !defined (HB_DONT_DEFINE_STDINT)
 
